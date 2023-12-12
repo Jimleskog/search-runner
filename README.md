978da197d6cfa736598cb6555da1536a9103a708
Jimleskog <124883061+Jimleskog@users.noreply.github.com>
2023-12-12 18:27:30 +0100

    first upload

---
+++ README.md
@@ -0,0 +1,32 @@
+# Search Runner Extension for VS Code
+
+This is the README for the "Search Runner" extension for Visual Studio Code. This extension provides a convenient way to search for translations in your codebase.
+
+## Features
+
+The Search Runner extension allows you to quickly search a specified document in the settings.
+
+- **Search Translation**: This command allows you to search in specified documents. FOr example if you have a large document with all translations then you want to convenient search that document with one click.
+
+![Search Translation](images/search-runner.png)
+
+## Requirements
+
+This extension requires Visual Studio Code to be installed.
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+- `search-runner.enable`: Enable/disable this extension.
+- `search-runner.documentname`: sets the path to the document that you want to search
+
+## Known Issues
+
+There are no known issues at the moment. If you encounter any problems, please open an issue on the [GitHub repository](https://github.com/yourusername/search-runner).
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of Search Runner.
---
+++ searchTranslations.js
@@ -0,0 +1,41 @@
+const vscode = require('vscode');
+const path = require('path');
+
+async function searchTranslation() {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return;
+    }
+
+    const documentName = vscode.workspace.getConfiguration().get('extension.searchTranslation.documentName');
+
+    if (!documentName) {
+        vscode.window.showErrorMessage('Document name is not specified in settings.');
+        return;
+    }
+
+    const dartScriptPath = path.join(vscode.workspace.rootPath || '', documentName);
+
+    try {
+        // Open the Dart script in the editor
+        const document = await vscode.workspace.openTextDocument(dartScriptPath);
+        await vscode.commands.executeCommand('editor.action.addSelectionToNextFindMatch');
+        await vscode.window.showTextDocument(document);
+        // Move to the first occurrence in the opened file
+        await vscode.commands.executeCommand('editor.action.nextMatchFindAction');
+
+
+
+
+    } catch (error) {
+        console.error(`Error opening Dart script: ${error.message}`);
+        vscode.window.showErrorMessage('Error opening Dart script. See the console for details.');
+    }
+    // } else {
+    //     vscode.window.showInformationMessage('No word selected for searching.');
+    // }
+}
+
+module.exports = {
+    searchTranslation
+};
